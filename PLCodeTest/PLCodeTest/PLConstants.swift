36dbd90af87d02216ac4e08a08ca909ff490c25f
Gordon Gao <yuangao0317@gmail.com>
2016-10-07 12:02:48 -0400

    add API endpoints constants

---
+++ PLCodeTest/PLCodeTest/PLConstants.swift
@@ -0,0 +1,16 @@
+//
+//  PLConstants.swift
+//  PLCodeTest
+//
+//  Created by GaoYuan on 10/7/16.
+//  Copyright © 2016 YuanGao. All rights reserved.
+//
+
+import Foundation
+
+enum API {
+    static let server  = "http://prolific-interview.herokuapp.com/57f7b32592f7ad0009dc8656/"
+    
+    static let books = server + "books/"
+    static let cleanBooks = server + "clean/"
+}
