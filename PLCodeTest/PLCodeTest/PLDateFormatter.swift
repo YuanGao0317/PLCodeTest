d55b0e7e3816e3e7f71e3a4f8ab72453ba4a9dee
Gordon Gao <yuangao0317@gmail.com>
2016-10-11 09:09:00 -0400

    remote server lastCheckedOut has its own format

--- PLCodeTest/PLCodeTest/PLDateFormatter.swift
+++ PLCodeTest/PLCodeTest/PLDateFormatter.swift
@@ -38,7 +38,7 @@ extension PLFormatter {
   func formatted(from string: String) -> String {
     if self == .stringToDate {
       let dateFormatter = DateFormatter()
-//      dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+//      dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss zzz"
       dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
       if let date = dateFormatter.date(from: string) {
         let dateFormatter = DateFormatter()
